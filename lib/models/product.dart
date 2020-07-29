036ef6273f13f4f1799bbf3de9a904931d135cff
Minav <33686146+Minav89@users.noreply.github.com>
2020-07-29 18:01:49 +0545

    Add files via upload

---
+++ lib/models/product.dart
@@ -0,0 +1,35 @@
+class Product {
+  final int id, price;
+  final String title, description, image;
+
+  Product({this.id, this.price, this.title, this.description, this.image});
+}
+
+// list of products
+// for our demo
+List<Product> products = [
+  Product(
+    id: 1,
+    price: 56,
+    title: "Puja Services",
+    image: "assets/images/pooja.png",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim",
+  ),
+  Product(
+    id: 4,
+    price: 68,
+    title: "Packages",
+    image: "assets/images/packages.png",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim",
+  ),
+  Product(
+    id: 9,
+    price: 39,
+    title: "Shop",
+    image: "assets/images/shop.png",
+    description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim",
+  ),
+];
---
+++ lib/screens/details/components/product_image.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../../../constants.dart';
+
+class ProductPoster extends StatelessWidget {
+  const ProductPoster({
+    Key key,
+    @required this.size,
+    this.image,
+  }) : super(key: key);
+
+  final Size size;
+  final String image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: kDefaultPadding),
+      // the height of this container is 80% of our width
+      height: size.width * 0.8,
+
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: <Widget>[
+          Container(
+            height: size.width * 0.65,
+            width: size.width * 0.6,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              shape: BoxShape.circle,
+            ),
+          ),
+          Image.asset(
+            image,
+            height: size.width * 0.65,
+            width: size.width * 0.65,
+            fit: BoxFit.cover,
+          ),
+        ],
+      ),
+    );
+  }
+}
